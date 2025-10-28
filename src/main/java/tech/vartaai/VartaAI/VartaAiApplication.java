f58b97628dd896b7f3ea0d8e315db2e431e7ed1e
SureshChoudhary1 <suresh.choudhary@vegapay.tech>
2025-10-28 16:15:14 +0530

    initial commit

---
+++ src/main/java/tech/vartaai/VartaAI/VartaAiApplication.java
@@ -0,0 +1,13 @@
+package tech.vartaai.VartaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VartaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VartaAiApplication.class, args);
+	}
+
+}
